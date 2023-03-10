edd02917ba6bb867da5a02fdcdc89b6937019d86
OMOCHI <satsukisama.mochi@gmail.com>
2023-03-11 02:38:36 +0900

    要件実装済み CSS適用前

---
+++ app/models/schedule.rb
@@ -0,0 +1,14 @@
+class Schedule < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :end_date_check
+  validates :memo, length: { maximum: 500 }
+
+  def end_date_check
+    if self.start_date > self.end_date
+      errors.add(:end_date, "は開始日より前の日付は選択出来ません。")
+    end
+  end
+
+end
